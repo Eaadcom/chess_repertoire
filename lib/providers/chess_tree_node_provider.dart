7a6093c8452293e40853471494fbfc8789c7d606
edwarddeen <eaadcom@gmail.com>
2025-11-24 23:50:19 +0100

    did some prep for saving the repertoire to a local db

--- lib/providers/chess_tree_node_provider.dart
+++ lib/providers/chess_tree_node_provider.dart
@@ -1,12 +1,18 @@
 import 'package:chess_repertoire/models/chess_tree_node.dart';
 import 'package:dartchess/dartchess.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 class ChessTreeNodeNotifier extends StateNotifier<Map<String, ChessTreeNode>> {
   ChessTreeNodeNotifier()
       : super({
           Chess.initial.fen: ChessTreeNode(
-              fen: Chess.initial.fen, playedMove: '', fromNodes: {}, nodePosition: Chess.initial)
+              fen: Chess.initial.fen,
+              playedMove: '',
+              fromNodes: {},
+              nodePosition: Chess.initial)
         });
 
   void addNodeToRegsitry(String fen, ChessTreeNode newChessTreeNode) {
@@ -14,6 +20,20 @@ class ChessTreeNodeNotifier extends StateNotifier<Map<String, ChessTreeNode>> {
       ...state,
       fen: newChessTreeNode,
     };
+  }
+
+  void saveNodesToDatabase() async {
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'repertoirePositions.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE repertoire_positions(fen TEXT PRIMARY KEY, playedMove TEXT, )',
+          // Requirements: saving the FEN, saving the references to FENS, saving the moves
+        );
+      },
+      version: 1,
+    );
   }
 
   Map<String, ChessTreeNode> getNodeRegistry() {
